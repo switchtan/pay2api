734007da55516872a84453d680270a69273fb0ac
guava <399852658@qq.com>
2019-07-13 10:53:47 +0800

    new

---
+++ gitini.sh
@@ -0,0 +1,9 @@
+git init
+git config --global user.name "guava"
+git config --global user.email "399852658@qq.com"
+git remote add origin https://switchtan:00gavaguava@github.com/switchtan/pay2api.git
+git pull origin master
+git fetch
+git remote -v
+git branch -r
+git checkout master
