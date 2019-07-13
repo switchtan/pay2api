2c6beff86f5b7126678f84f6a413942ff10e45d2
guava <399852658@qq.com>
2019-07-13 18:29:06 +0800

    new

--- html/dbconfig.php
+++ html/dbconfig.php
@@ -1,31 +1,36 @@
-<?php	
-	require 'rb.php';
+<?php
+require 'rb.php';
 
-	R::setup( 'mysql:host=127.0.0.1;dbname=wxpay',
-        'root', 'guavaguava00' ); //for both mysql or mariaDB
+R::setup(
+    'mysql:host=127.0.0.1;dbname=wxpay',
+    'root',
+    'guavaguava00'
+); //for both mysql or mariaDB
 
-    function urlparams($params){
-        $sign = '';
-        foreach ($params AS $key => $val) {
-            if ($val == '') continue;
-            if ($key != 'sign') {
-                if ($sign != '') {
-                    $sign .= "&";
-                    // $urls .= "&";
-                }
-                $sign .= "$key=$val"; //拼接为url参数形式
+function urlparams($params)
+{
+    $sign = '';
+    foreach ($params as $key => $val) {
+        if ($val == '') continue;
+        if ($key != 'sign') {
+            if ($sign != '') {
+                $sign .= "&";
+                // $urls .= "&";
             }
+            $sign .= "$key=$val"; //拼接为url参数形式
         }
-        return $sign;
     }
+    return $sign;
+}
 
-    function client_sign($appid,$appkey,$timestamp){
-        $request_objs = array(
-          "appid"  =>  $appod,//APPID号码
-          "appkey" =>  $appke,
-          "timestamp"=>$timestamp
-        );
+function client_sign($appid, $appkey, $timestamp)
+{
+    $request_objs = array(
+        "appid"  =>  $appid, //APPID号码
+        "appkey" =>  $appkey,
+        "timestamp" => $timestamp
+    );
 
-        $token3 = md5(urlparams($request_objs));
-        return $token3;
-    }
+    $token3 = md5(urlparams($request_objs));
+    return $token3;
+}
--- html/getMoney.php
+++ html/getMoney.php
@@ -1,32 +1,30 @@
-<?php	
-	header("Access-Control-Allow-Origin:*");
-	/*星号表示所有的域都可以接受，*/
-	header("Access-Control-Allow-Methods:GET,POST");
- 	require 'dbconfig.php';
-		
+<?php
+header("Access-Control-Allow-Origin:*");
+/*星号表示所有的域都可以接受，*/
+header("Access-Control-Allow-Methods:GET,POST");
+require 'dbconfig.php';
 
-	if(isset($_GET["mid"])   && isset($_GET["money"]) ){
-		
-		$pay_record = R::dispense( 'payrecord' );
-		$pay_record->mid = $_GET["mid"];
-		$pay_record->money=$_GET["money"];
-		$pay_record->datenow=$_GET["date"];
-		$pay_record->appid=$_GET["appid"];
-		// $pay_record->order_number="" ;
 
-		$siteuser_obj = R::findOne( 'siteuser', ' appid = ? ', [ $_REQUEST['appid']] );
-        // echo 'appkey:'.$siteuser_obj['appkey'].'<br>';
+if (isset($_GET["mid"])   && isset($_GET["money"])) {
 
-		$token=$_REQUEST['token'];
-		$token_local=client_sign($_REQUEST["appid"], $siteuser_obj['appkey'], $_REQUEST["date"] );
-		if($token_local == $token ){
-		    $id = R::store( $pay_record );
-		    echo json_encode(array('state'=>'1'));
-		}else{
-		    echo json_encode(array('error'=>'token no match,maybe appkey error','state'=>'0'));
-		}
+	$pay_record = R::dispense('payrecord');
+	$pay_record->mid = $_GET["mid"];
+	$pay_record->money = $_GET["money"];
+	$pay_record->datenow = $_GET["date"];
+	$pay_record->appid = $_GET["appid"];
+	// $pay_record->order_number="" ;
 
-	}else{
-		echo json_encode(array('error'=>'querys no match','state'=>'0'));
+	$siteuser_obj = R::findOne('siteuser', ' appid = ? ', [$_REQUEST['appid']]);
+	// echo 'appkey:'.$siteuser_obj['appkey'].'<br>';
+
+	$token = $_REQUEST['token'];
+	$token_local = client_sign($_REQUEST["appid"], $siteuser_obj['appkey'], $_REQUEST["date"]);
+	if ($token_local == $token) {
+		$id = R::store($pay_record);
+		echo json_encode(array('state' => '1'));
+	} else {
+		echo json_encode(array('error' => 'token no match,maybe appkey error', 'state' => '0'));
 	}
-	?>
+} else {
+	echo json_encode(array('error' => 'querys no match', 'state' => '0'));
+}
